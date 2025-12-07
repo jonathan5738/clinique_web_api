fd9b9dcbc4d3b1cad45779e720595d392fe97dc1
jonathan5738 <jnakahonda@gmail.com>
2025-12-07 17:36:44 +0100

    trying to run azure

--- obj/Debug/net8.0/CliniqueBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/CliniqueBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CliniqueBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e28614acdbe607f67ea3505b8d731f0a66e921d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b69ae3d279fc955b9e58f73f6cef719dbaee6bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CliniqueBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CliniqueBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
